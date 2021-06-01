44aae5f078096b70452764a610ac0804a3ee6e33
Amaury Tissot Mazy <tissotmazy@gmail.com>
2021-06-01 21:30:49 +0200

    Suite setup programme arduino

--- Programme_Solarius/Programme_Solarius.ino
+++ Programme_Solarius/Programme_Solarius.ino
@@ -3,24 +3,40 @@
 Servo servorotate;
 Servo servoinc;
 
-int capteur_lum = 0; // capteur branché sur le port 0
-int analog_lum; // valeur analogique envoyé par le capteur
+int capteur_lum1 = 0; // capteur branché sur le port A0
+int analog_lum1; // valeur analogique envoyé par le capteur
+
+int capteur_lum2 = 1; // capteur branché sur le port A1
+int analog_lum2; // valeur analogique envoyé par le capteur
+
+int capteur_lum3 = 2; // capteur branché sur le port A2
+int analog_lum3; // valeur analogique envoyé par le capteur
+
+int capteur_lum4 = 3; // capteur branché sur le port A3
+int analog_lum4; // valeur analogique envoyé par le capteur
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600); // démarrer la liaison série
   servorotate.attach(8);
   servoinc.attach(9);
-  pinMode(A0,INPUT_PULLUP); // attache les potentiomètres à la bonne pin
+  pinMode(A0,INPUT_PULLUP); // attache les capteur de luminosité à la bonne pin
   pinMode(A1,INPUT_PULLUP);
 }
 
 void loop() {
-  analog_lum = analogRead(capteur_lum); // lecture de la valeur analogique, qu'on enregistre dans analog_lum
-  Serial.print("Valeur luminosité = ");
-  Serial.print(analog_lum); 
-  Serial.println("");
-  delay(1000);
+  analog_lum1 = analogRead(capteur_lum1); // lecture de la valeur analogique, qu'on enregistre dans analog_lum
+  Serial.print("Valeur luminosité1 = ");
+  
+  analog_lum2 = analogRead(capteur_lum2);
+  Serial.print("Valeur luminosité2 = ");
+  
+  analog_lum3 = analogRead(capteur_lum3);
+  Serial.print("Valeur luminosité3 = ");
+  
+  analog_lum4 = analogRead(capteur_lum4);
+  Serial.print("Valeur luminosité4 = ");
+  
   // put your main code here, to run repeatedly:
 
 }
